4cc56298e1e0f15beffda475f900f6afdf5bd0b9
pritulaziah <ziahherzen@gmail.com>
2026-05-31 19:20:46 +0500

    feat: read and print log file line by line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use anyhow::Result;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    file_path: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let file = File::open(&args.file_path)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        println!("{}", line);
+    }
+
+    Ok(())
 }
